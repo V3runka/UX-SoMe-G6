626043d93cb14f2f407698b5643cf33717fdae84
Veronika Šilhová <eaavsi@students.eaaa.dk>
2019-03-28 16:23:03 +0100

    Add comments

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Scroll to the bottom of the page.
 window.scrollTo(0, document.body.scrollHeight);
 
-// Apply parallax effect on the SVGs which has the '.layer' class (hill1, hill2, castle). Code is taken from https://www.youtube.com/watch?v=itg8hiG3jFU
+// Apply parallax effect on the SVGs which has the '.layer' class (hill1, hill2, castle). Code is taken from https://www.youtube.com/watch?v=itg8hiG3jFU and modify for our needs.
 $(window).scroll(function () {
     const height = $(document).height();
     const top = $(window).scrollTop() + $(window).height();
@@ -10,6 +10,7 @@ $(window).scroll(function () {
     $('.layer').each(function () {
         var layer = $(this);
         var dataSpeed = layer.data('speed');
+        // offsetY is the number of pixels by which the elemet should move
         var offsetY = -(scrollTop * dataSpeed);
         var translate = 'translate3d(0, ' + offsetY + 'px, 0)';
         layer.css('transform', translate);
