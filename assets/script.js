fff312174db549288f058fb576126d78091372bc
Veronika Šilhová <eaavsi@students.eaaa.dk>
2019-03-28 16:15:34 +0100

    Add comments

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
-// Apply parallax effect on the SVGs which has the '.layer' class (hill1, hill2, castle). Code is taken from https://www.youtube.com/watch?v=itg8hiG3jFU
+// Scroll to the bottom of the page.
 window.scrollTo(0, document.body.scrollHeight);
 
+// Apply parallax effect on the SVGs which has the '.layer' class (hill1, hill2, castle). Code is taken from https://www.youtube.com/watch?v=itg8hiG3jFU
 $(window).scroll(function () {
     const height = $(document).height();
     const top = $(window).scrollTop() + $(window).height();
