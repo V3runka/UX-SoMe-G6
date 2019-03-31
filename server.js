2ff3f5277a9235f4b82ba4a5968bc5e6eff31a73
Veronika Šilhová <eaavsi@students.eaaa.dk>
2019-03-31 20:10:21 +0200

    deleting meta data files from mac

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('assets'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/remote-control', function(req, res) {
+    res.sendFile(__dirname + '/remote-control.html');
+});
+
+io.on('connection', socket => {
+    // socket.broadcast.emit('connected');
+
+    socket.on('buttonPress', floorId => {
+        io.emit('scrollToFloor', floorId);
+    });
+
+    // socket.on('disconnect', () => {
+    //     console.log('client disconnected');
+    // });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
