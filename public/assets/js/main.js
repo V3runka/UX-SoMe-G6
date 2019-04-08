a7fdf2ad01a9382e5460a4f72f798e2233b07fef
Veronika Šilhová <eaavsi@students.eaaa.dk>
2019-04-08 13:24:27 +0200

    add dragon and frame

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -18,30 +18,114 @@ $(window).scroll(function () {
     });
 });
 
-// I used Event Bubbling. See for more: https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
 document.addEventListener('click', function (event) {
+    // I used Event Bubbling. See for more: https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
     if (event.target.classList.contains('cloud')) {
         event.target.parentElement.style.display = "none";
         setTimeout(function () {
             event.target.parentElement.style.display = "block";
         }, 5000);
     }
-}, false);
 
-document.addEventListener('click', function (event) {
     if (event.target.classList.contains('change-color-a')) {
         event.target.classList.toggle("bird-color-blue");
     }
-}, false);
 
-document.addEventListener('click', function (event) {
     if (event.target.classList.contains('change-color-b')) {
         event.target.classList.toggle("bird-color-red");
     }
-}, false);
 
-document.addEventListener('click', function (event) {
     if (event.target.classList.contains('change-color-c')) {
         event.target.classList.toggle("bird-color-yellow");
     }
 }, false);
+
+
+// Script for Dragon and Lift animation
+
+// Animation layers
+// Layer 1: Frame
+const frame = {
+    container: document.getElementById('dragon-frame'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: 'assets/data/dragon-animation/frame.json'
+};
+
+// Layer 2: Wheel
+const wheel = {
+    container: document.getElementById('dragon-wheel'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/data/dragon-animation/wheel.json'
+};
+
+
+// Layer 3: Tail
+const tail = {
+    container: document.getElementById('dragon-tail'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/data/dragon-animation/tail.json'
+};
+
+// Layer 4: Wing
+const wing = {
+    container: document.getElementById('dragon-wing'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/data/dragon-animation/wing.json'
+};
+
+// Layer 5: Body, left arm and leg
+const body = {
+    container: document.getElementById('dragon-body'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/data/dragon-animation/body.json'
+};
+
+
+// Layer 6: Right leg
+const leg = {
+    container: document.getElementById('dragon-leg'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/data/dragon-animation/leg.json'
+};
+
+// Layer 7: Right arm
+const arm = {
+    container: document.getElementById('dragon-arm'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/data/dragon-animation/arm.json'
+};
+
+// Layer 8: Head, facial expressions and fire
+const head = {
+    container: document.getElementById('dragon-head'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/data/dragon-animation/face.json'
+};
+
+lottie.loadAnimation(frame);
+lottie.loadAnimation(wheel);
+lottie.loadAnimation(tail);
+lottie.loadAnimation(wing);
+lottie.loadAnimation(body);
+lottie.loadAnimation(leg);
+lottie.loadAnimation(arm);
+lottie.loadAnimation(head);
+
+// Plays animations
+lottie.play();
