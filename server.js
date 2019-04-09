a92b1244215ac97e9c516acf2f10b1b7d730890e
Veronika Šilhová <eaavsi@students.eaaa.dk>
2019-04-09 16:16:26 +0200

    setup heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -25,6 +26,6 @@ io.on('connection', socket => {
     // });
 });
 
-http.listen(3000, () => {
+http.listen(port, () => {
     console.log('listening on *:3000');
 });
